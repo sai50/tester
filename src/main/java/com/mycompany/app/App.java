4608a3d5473a384915f881e69fdc69c4da637191
saiteja50 <madabhushisaiteja@gmail.com>
2016-08-28 10:12:06 +0530

    Adding Files

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+public class App 
+{
+  public static void main(String[] args) {
+   
+     
+     boolean blnStatus = true;
+     
+     if(blnStatus)
+       System.out.println("Status is true");
+  }
+}
